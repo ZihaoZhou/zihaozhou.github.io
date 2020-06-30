2d4e2548c94b411c540e220546b35964ec70e474
Zihao Zhou <ziz244@ucsd.edu>
2020-06-30 01:55:30 -0700

    Test joystick

---
+++ script.js
@@ -0,0 +1,11 @@
+var joy = nipplejs.create({
+  zone: document.getElementById('joy'),
+  size: 175,
+  mode: 'static',
+  color: 'transparent',
+  restOpacity: 1,
+  position: {
+    left: '50%',
+    top: '50%'
+  }
+});
